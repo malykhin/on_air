b83488e39ef4aa258ba3972d21123e2e75e12a75
Denys Malykhin <malykhin@ukr.net>
2022-08-11 00:03:47 -0700

    use log stream

--- update_mic_status_mac.js
+++ update_mic_status_mac.js
@@ -1,49 +1,37 @@
 #!/usr/bin/env node
 
-const { promisify } = require('util')
-const { exec, execSync } = require('child_process')
+const { spawn } = require('child_process')
 const { request } = require('http')
 
-const execPromise = promisify(exec)
-
 const SIGN_ADDRESS = 'http://10.0.0.119:80'
-const POLL_TIMEOUT = 1000
 
-const GET_MIC_STATUS_COMMAND =
-  'ioreg -c AppleHDAEngineInput | grep "Digital Mic"'
+const GET_MIC_RELATED_STREAM_COMMAND = '/usr/bin/log'
+const GET_MIC_RELATED_STREAM_ARGS = [
+  'stream',
+  '--predicate',
+  'eventMessage contains "BuiltInHeadphoneInputDevice"',
+]
 
 const url = new URL(SIGN_ADDRESS)
 
-const getValue = (str) =>
-  Number(str.split(',').pop().replace('>', '').replace('retain', '').trim())
+const stream = spawn(
+  GET_MIC_RELATED_STREAM_COMMAND,
+  GET_MIC_RELATED_STREAM_ARGS,
+)
 
-let prevValue = getValue(execSync(GET_MIC_STATUS_COMMAND).toString())
+stream.stdout.on('data', async (data) => {
+  const message = data.toString()
 
-console.log('initial value', prevValue)
+  const isStarting = message.lastIndexOf('starting') != -1
+  const isStopping = message.lastIndexOf('stopping') != -1
 
-const valuesMap = {
-  10: 'led_off',
-  12: 'led_on',
-}
+  if (!isStarting && !isStopping) {
+    return
+  }
 
-async function run() {
+  const payload = isStarting ? 'led_on' : 'led_off'
+
   try {
-    const { stdout } = await execPromise(GET_MIC_STATUS_COMMAND)
-
-    const value = getValue(stdout)
-
-    const data = valuesMap[value]
-
-    if (value === prevValue || !data) {
-      return
-    }
-
-    console.log('prevValue', prevValue)
-    console.log('value', value)
-    console.log('request data', data)
-
-    prevValue = value
-
     await new Promise((resolve, reject) => {
       const options = {
         hostname: url.host,
@@ -52,32 +40,42 @@ async function run() {
         headers: {
           'Content-Type': 'text/plain',
           Connection: 'keep-alive',
-          'Content-Length': Buffer.byteLength(data),
+          'Content-Length': Buffer.byteLength(payload),
         },
       }
+
       const req = request(options, (res) => {
         res.setEncoding('utf8')
+
         res.on('data', (data) => {
           console.log('response data', data)
         })
+
         res.on('end', () => {
           resolve()
         })
+
         res.on('error', (error) => {
           reject(error)
         })
       })
+
       req.on('error', (error) => {
         reject(error)
       })
-      req.write(data)
+
+      req.write(payload)
       req.end()
     })
   } catch (error) {
     console.log('error', error)
-  } finally {
-    setTimeout(run, POLL_TIMEOUT)
   }
-}
+})
 
-run()
+stream.stderr.on('data', (data) => {
+  console.log('stderr', data.toString())
+})
+
+stream.on('close', () => {
+  console.log('closed')
+})
